aae50fb7288a7f5a9b41729d8d2e829aee3a4384
Ana Davis <anac.davis@gmail.com>
2019-09-25 22:03:44 -0700

    changes testing

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,9 +7,10 @@ var counter = 10;
 var myQuestions = [
   {
     question: "What causes the ocean's tides?",
-    answers: ["Orbit of the earth aroun the sun", "Gravitational pull of the sun", "Gravitational pull of the moon", "Rotation of the earth"],
+    answers: ["Orbit of the earth aroun the sun \nGravitational pull of the sun \nGravitational pull of the moon \nRotation of the earth"],
     correctAnswer: "Gravitational pull of the sun"
   },
+  
 
   {
     question: "The water _________ in northern oceans",
@@ -17,59 +18,74 @@ var myQuestions = [
     correctAnswer: "Spins clockwise"
   },
 
-{
-  question: "Which was the new ocean named in 2000?",
-  answers: ["Artic Ocean", "Pacific Ocean", "Southern Ocean", "Indian Ocean"],
-  correctAnswer: "Southern Ocean"
-},
-  
-{
-  question: "How much of the world is covered by water?",
-  answers: ["70 percent", "50 percent", "30 percent", "15 percent"],
-  correctAnswer: "70 percent"
-},
- 
-{
-  question: "How many oceans are there today",
-  answers: ["3", "4", "5", "6"],
-  correctAnswer: "5"
-},
+  {
+    question: "Which was the new ocean named in 2000?",
+    answers: ["Artic Ocean", "Pacific Ocean", "Southern Ocean", "Indian Ocean"],
+    correctAnswer: "Southern Ocean"
+  },
 
-{
-  question: "Waves are caused by _________",
-  answers: ["Gravitational pull of the moon", "Temperature", "Rain", "Wind"],
-  correctAnswer: "Wind"
-},
+  {
+    question: "How much of the world is covered by water?",
+    answers: ["70 percent", "50 percent", "30 percent", "15 percent"],
+    correctAnswer: "70 percent"
+  },
 
-{
-  question: "One cubic foot of ocean water contains over ______ of salt",
-  answers: ["4 pounds", "2 pounds", "1 pound", "5 pounds"],
+  {
+    question: "How many oceans are there today",
+    answers: ["3", "4", "5", "6"],
+    correctAnswer: "5"
+  },
+
+  {
+    question: "Waves are caused by _________",
+    answers: ["Gravitational pull of the moon", "Temperature", "Rain", "Wind"],
+    correctAnswer: "Wind"
+  },
+
+  {
+    question: "One cubic foot of ocean water contains over ______ of salt",
+    answers: ["4 pounds", "2 pounds", "1 pound", "5 pounds"],
     correctAnswer: "2 pounds"
-},
+  },
 
-{
-  question: "The Red Sean and _____ have the saltiest water of all open seas and oceans",
-  answers: ["Persian Gulf", "Caspian Sea", "Dead Sea", "Atlantic Ocean"],
-  correctAnswer: "Persian Gulf"
-}
+  {
+    question: "The Red Sean and _____ have the saltiest water of all open seas and oceans",
+    answers: ["Persian Gulf", "Caspian Sea", "Dead Sea", "Atlantic Ocean"],
+    correctAnswer: "Persian Gulf"
+  }
 ]
 
-function startTimer(){
+// ---------------Quiz ------------------
+function buildQuiz(){
+  var output =[];
+  myQuestions.forEach(
+    (currentQuestion, questionNumber) => {
+      var answers = [];
+      for(currentQuestion.answers[0]){
+        answers.push('')
+      }
+    }
+  )
+}
+
+
+// -------------Timer---------------------
+function startTimer() {
   var counter = 10;
-  setInterval(function(){
+  setInterval(function () {
     counter--;
-    if(counter >= 0){
+    if (counter >= 0) {
       span = document.getElementById("count");
       span.innerHTML = counter;
     }
-    if(counter === 0){
+    if (counter === 0) {
       clearInterval(counter);
     }
   }, 1000);
 }
-$("#startClock").click(function(){
+
+$("#startClock").click(function () {
   startTimer();
-  console.log(counter)
 });
 
 
