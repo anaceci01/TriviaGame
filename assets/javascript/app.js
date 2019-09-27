3a33ae71f608dc0fdb874d7b95caee7f27fb4937
Ana Davis <anac.davis@gmail.com>
2019-09-26 18:40:43 -0700

    changed layout of box

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,72 +1,26 @@
 //var timer = 10;
 var intervalID;
-var counter = 10;
+var currenQuestion = 0;
+var score = 0;
+var lost = 0;
+// holds value of reset clock
+var timer;
 
 
 // Questions
-var myQuestions = [
-  {
-    question: "What causes the ocean's tides?",
-    answers: ["Orbit of the earth aroun the sun \nGravitational pull of the sun \nGravitational pull of the moon \nRotation of the earth"],
-    correctAnswer: "Gravitational pull of the sun"
-  },
-  
-
-  {
-    question: "The water _________ in northern oceans",
-    answers: ["Is saltiest", "Spins clockwise", "Freezes often", "Spins counterclockwise"],
-    correctAnswer: "Spins clockwise"
-  },
-
-  {
-    question: "Which was the new ocean named in 2000?",
-    answers: ["Artic Ocean", "Pacific Ocean", "Southern Ocean", "Indian Ocean"],
-    correctAnswer: "Southern Ocean"
-  },
-
-  {
-    question: "How much of the world is covered by water?",
-    answers: ["70 percent", "50 percent", "30 percent", "15 percent"],
-    correctAnswer: "70 percent"
-  },
-
-  {
-    question: "How many oceans are there today",
-    answers: ["3", "4", "5", "6"],
-    correctAnswer: "5"
-  },
-
-  {
-    question: "Waves are caused by _________",
-    answers: ["Gravitational pull of the moon", "Temperature", "Rain", "Wind"],
-    correctAnswer: "Wind"
-  },
-
-  {
-    question: "One cubic foot of ocean water contains over ______ of salt",
-    answers: ["4 pounds", "2 pounds", "1 pound", "5 pounds"],
-    correctAnswer: "2 pounds"
-  },
-
-  {
-    question: "The Red Sean and _____ have the saltiest water of all open seas and oceans",
-    answers: ["Persian Gulf", "Caspian Sea", "Dead Sea", "Atlantic Ocean"],
-    correctAnswer: "Persian Gulf"
-  }
-]
 
 // ---------------Quiz ------------------
-function buildQuiz(){
-  var output =[];
-  myQuestions.forEach(
-    (currentQuestion, questionNumber) => {
-      var answers = [];
-      for(currentQuestion.answers[0]){
-        answers.push('')
-      }
-    }
-  )
-}
+// function buildQuiz(){
+//   var output =[];
+//   myQuestions.forEach(
+//     (currentQuestion, questionNumber) => {
+//       var answers = [];
+//       for(currentQuestion.answers[0]){
+//         answers.push('')
+//       }
+//     }
+//   )
+// }
 
 
 // -------------Timer---------------------
@@ -84,7 +38,7 @@ function startTimer() {
   }, 1000);
 }
 
-$("#startClock").click(function () {
+$("#start-btn btn").click(function () {
   startTimer();
 });
 
