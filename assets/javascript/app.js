cefdcd502a19304d61ec7c0975aa82a6ba9682d2
Ana Davis <anac.davis@gmail.com>
2019-10-03 18:30:37 -0700

    question boxes and functions added

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,58 +1,51 @@
-//var timer = 10;
-var intervalID;
-var currenQuestion = 0;
-var score = 0;
-var lost = 0;
-// holds value of reset clock
-var timer;
+// -----------------VARIABLES ---------------------
+const startButton = document.getElementById('start-btn')
+const questionContainerElement = document.getElementById('question-container')
+let questionElement = document.getElementById('question')
+let answerButtonsElement = document.getElementById('answer-buttons')
+let shuffleQuestions, currentQuestion
 
+startButton.addEventListener('click', startGame)
 
-// Questions
+// -----------------FUNCTIONS ---------------------
+function startGame () {
 
-// ---------------Quiz ------------------
-// function buildQuiz(){
-//   var output =[];
-//   myQuestions.forEach(
-//     (currentQuestion, questionNumber) => {
-//       var answers = [];
-//       for(currentQuestion.answers[0]){
-//         answers.push('')
-//       }
-//     }
-//   )
-// }
-
-
-// -------------Timer---------------------
-function startTimer() {
-  var counter = 10;
-  setInterval(function () {
-    counter--;
-    if (counter >= 0) {
-      span = document.getElementById("count");
-      span.innerHTML = counter;
-    }
-    if (counter === 0) {
-      clearInterval(counter);
-    }
-  }, 1000);
+startButton.classList.add("hide")
+shuffleQuestions = questions.sort(() => Math.random() - .5)
+currentQuestion = 0
+questionContainerElement.classList.remove('hide')
+nextQuestion()
 }
 
-$("#start-btn btn").click(function () {
-  startTimer();
-});
+function nextQuestion () {
+  showQuestion(shuffleQuestions[currentQuestion])
+}
+//
+function showQuestion (question){
+  questionElement.innerText = question.question
+  question.choices.forEach( answer => {
+    const button = document.createElement('button')
+    button.innerText =answer.text
+    button.classicList.add()
+    if(answer.correct) {
+      button.dataset.correct = answer.correct
+    }
+    button.addEventListener('click', selectAnswer)
+    answerBttnElement.appendChild(button)
+  })
+}
 
+function selectAnswer () {
 
+}
 
+const questions = [
+  {
+  question: "Which was the first nation to give women the right to vote?",
+  choices: [
+    {text: "New Zealand", correct: true},
+    {text: "France", correct: false},
+  ]
+}
 
-
-
-
-//---------Pseudo Code--------------//
-//---------------------------------//
-// Time Remaining re-starts after each question is answered
-// Each questions has a multiple choice answer. 
-//The user then, clicks on an answer and a "correct" or "incorrect" text pops up along with a GIF
-//New question loads
-//Once all questions have been answered. the number of correct answers or incorrect answer will display
-// The user has  an option to Start over or Play again.
+]
